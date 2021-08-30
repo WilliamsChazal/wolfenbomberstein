5f7b501666dbf56b33b6489fe69478c769a6deb3
williams <w.chazal@codeur.online>
2021-08-30 14:58:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>WolfenBomberStein</title>
+</head>
+<body>
+    <div class="container_titre">
+
+    <div class="titre"><H1>WolfenBomberStein</H1></div>
+
+    </div>
+    
+
+
+
+    <section>
+  <div class="container">
+
+      <div class="box">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <div class="content">
+        <div id="gameContainer">
+        <div id="player"></div>
+        <div id="bomb"></div>
+    </div>
+        </div>
+        
+      </div>
+
+  </div>
+</section>
+
+
+
+
+
+<script src="scripts/badguy.js"></script>
+<script src="scripts/boss.js"></script>
+<script src="scripts/bomb.js"></script>
+<script src="scripts/main.js"></script>
+
+
+
+
+</body>
+</html>
---
+++ scripts/boss.js
@@ -0,0 +1,104 @@
+let bossWalk = 0;
+
+
+function addBoss(max){
+
+    for (let i = 0; i < max; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'boss')
+        div.setAttribute('id', 'boss-'+i)
+        document.getElementById('gameContainer').appendChild(div);
+        positionEnemies(div);
+    }
+}
+function getRandomPosition(){
+    let randomX = Math.floor(Math.random()*24);
+    let randomY = Math.floor(Math.random()*24);
+     while (randomX < 2 || randomY <2) { 
+         randomX = Math.floor(Math.random()*24);
+         randomY = Math.floor(Math.random()*24);  
+    }
+    return[randomX,randomY];
+}
+function positionEnemies(boss){
+    let randomPosition = getRandomPosition();
+    let x = randomPosition[0]*12;
+    let y = randomPosition[1]*12;
+    boss.style.top = x + 'px';
+    boss.style.left = y + 'px';
+}
+
+setInterval(mouvEnemies, 500)
+
+function mouvEnemies(){
+    let doomGuy = document.getElementById('player');
+    let boss = document.getElementsByClassName('boss');
+
+    for (let i = 0; i < boss.length; i++){
+        let random = Math.floor(Math.random()*24);
+        
+switch (random) {
+    case 0: 
+    if(boss[i].offsetLeft <=696 ){
+        boss[i].style.top = boss[i].offsetTop +40 +'px';
+        boss[i].style.left = boss[i].offsetLeft  +'px';
+        
+    }
+        break;
+    case 1: 
+    if(boss[i].offsetTop <=696 ){
+        boss[i].style.top = boss[i].offsetTop  +'px';
+        boss[i].style.left = boss[i].offsetLeft +40 +'px';
+    }
+        break;
+    case 2: 
+    if( boss[i].offsetLeft >=80 && boss[i].offsetTop >=24 ){
+        boss[i].style.top = boss[i].offsetTop  +'px' ;
+        boss[i].style.left = boss[i].offsetLeft -40 +'px';
+    }  
+        break;
+    case 3: 
+    if(boss[i].offsetTop >=80 ){
+        boss[i].style.top = boss[i].offsetTop -40 +'px' ;
+        boss[i].style.left = boss[i].offsetLeft  +'px';
+    }   
+        break;
+    
+    default:
+        
+}
+
+
+    }
+    
+for (let i = 0; i < boss.length; i++) {
+            if(colisionDetect (doomGuy,doomGuy.offsetLeft, doomGuy.offsetTop, (boss[i]))){
+               /*  boss.backgroundImage="url('assets/mechants/demon-2.png')" */
+                doomGuyDead();
+                setTimeout(()=>alert("GAME OVER"),document.location.reload(),750);
+            }
+            
+        }
+        
+}
+
+/* const doomGuy = document.getElementById('doomGuy');
+const boss = document.getElementsByClassName('boss');
+colisionDetect(doomGuy.offsetLeft, doomGuy.offsetTop, boss [0]);
+function colisionDetect(x, y, boss){
+    if ((y < boss.offsetTop + boss.offsetHeight &&
+        y + doomGuy.offsetHeight > boss.offsetTop) &&
+        (x < boss.offsetLeft + boss.offsetWidth &&
+        x + doomGuy.offsetWidth > boss.offsetLeft)) {
+            console.log('boom'); 
+        return true;} 
+    else {
+        return false}
+          
+} */
+
+/*  
+    if (boss.offsetTop + enemiesSize < doomGuy.offsetTop + doomGuySize && boss.offsetLeft + enemiesSize < doomGuy.offsetLeft + doomGuySize){
+   https://developer.mozilla.org/fr/docs/Games/Techniques/2D_collision_detection 
+    */
+
